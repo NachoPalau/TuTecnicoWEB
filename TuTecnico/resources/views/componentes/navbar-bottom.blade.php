6a36a68c49e01c1b178fbc36d37bbfa97b11b29d
nachopalau <nacho.palau3001@gmail.com>
2025-03-20 12:12:52 +0100

    primer blade (servicios) responsive con complementos navbar implementado

---
+++ TuTecnico/resources/views/componentes/navbar-bottom.blade.php
@@ -0,0 +1,16 @@
+
+<div class="bottom-nav d-flex d-lg-none justify-content-around">
+    <a href="#" class="text-decoration-none text-dark text-center">
+      <img src="IMG/navbar-2/servicios.png" alt="Servicios">
+      <div>Servicios</div>
+    </a>
+    <a href="#" class="text-decoration-none text-dark text-center">
+      <img src="IMG/navbar-2/calendario.png" alt="Reservas">
+      <div>Reservas</div>
+    </a>
+    <a href="#" class="text-decoration-none text-dark text-center">
+      <img src="IMG/navbar-2/chat.png" alt="Mensajes">
+      <div>Mensajes</div>
+    </a>
+  </div> 
+  
---
+++ TuTecnico/resources/views/componentes/navbar-top.blade.php
@@ -0,0 +1,22 @@
+<nav class="navbar-top navbar bg-white shadow fixed-top">
+  <div class="container-fluid d-flex justify-content-between align-items-center">
+    <!-- Logo (visible solo en desktop) -->
+    <img src="IMG/logo/TuTecnicoG.png" alt="Logo de la empresa" class="d-none d-lg-block " height="55">
+
+    <!-- navbar-móvil (visible solo en móviles y tablets) -->
+    <div class="navbar-left d-flex d-lg-none align-items-center">
+      <span class="bold-text fw-bold me-2">Calle Cirilo Amoros</span>
+      <img src="IMG/navbar-1/flechita.png" alt="Desplegar">
+    </div>
+
+    <!-- Tres divs (Servicios, Reservas, Mensajes) - Solo en desktop -->
+    <div class="d-none d-lg-flex justify-content-center align-items-center flex-grow-1">
+      <div class="mx-5">Servicios</div>
+      <div class="mx-5">Reservas</div>
+      <div class="mx-5">Mensajes</div>
+    </div>
+
+    <!-- Login (visible en todos los dispositivos) -->
+    <img src="IMG/navbar-1/login.png" alt="Perfil" class="profile-icon" height="35">
+  </div>
+</nav>
