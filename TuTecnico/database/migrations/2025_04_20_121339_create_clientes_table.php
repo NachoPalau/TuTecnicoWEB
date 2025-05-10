6637108dcb9868c5ae650aa14dfe57335977dbac
Nacho Palau <147215013+NachoPalau@users.noreply.github.com>
2025-05-10 17:00:44 +0200

    servicio de cada profesional

--- TuTecnico/database/migrations/2025_04_20_121339_create_clientes_table.php
+++ TuTecnico/database/migrations/2025_04_20_121339_create_clientes_table.php
@@ -4,18 +4,28 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up() {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
         Schema::create('clientes', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('email')->unique();
-            $table->string('telefono')->nullable();
             $table->timestamps();
         });
     }
 
-    public function down() {
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
         Schema::dropIfExists('clientes');
     }
 };
